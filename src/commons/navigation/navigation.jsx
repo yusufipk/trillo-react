e09f62bb392915f51b8e42f993c7f3d1f8de9840
Yusuf Ipek <yusufipek@protonmail.com>
2021-05-09 12:37:40 +0300

    styled navbar

--- src/commons/navigation/navigation.jsx
+++ src/commons/navigation/navigation.jsx
@@ -1,5 +1,48 @@
+import sprite from "../img/sprite.svg";
+
 function Nav() {
-  return <nav className="sidebar">Navigation</nav>;
+  return (
+    <nav className="sidebar">
+      <ul className="sidebar__side-nav">
+        <li className="sidebar__side-nav--item sidebar__side-nav--item--active">
+          <a href="#" className="sidebar__side-nav--link">
+            <svg className="sidebar__side-nav--icon">
+              <use href={`${sprite}#icon-home`}></use>
+            </svg>
+            <span>Hotel</span>
+          </a>
+        </li>
+
+        <li className="sidebar__side-nav--item">
+          <a href="#" className="sidebar__side-nav--link">
+            <svg className="sidebar__side-nav--icon">
+              <use href={`${sprite}#icon-aircraft-take-off`}></use>
+            </svg>
+            <span>Flight</span>
+          </a>
+        </li>
+
+        <li className="sidebar__side-nav--item">
+          <a href="#" className="sidebar__side-nav--link">
+            <svg className="sidebar__side-nav--icon">
+              <use href={`${sprite}#icon-key`}></use>
+            </svg>
+            <span>Car rental</span>
+          </a>
+        </li>
+
+        <li className="sidebar__side-nav--item">
+          <a href="#" className="sidebar__side-nav--link">
+            <svg className="sidebar__side-nav--icon">
+              <use href={`${sprite}#icon-map`}></use>
+            </svg>
+            <span>Tours</span>
+          </a>
+        </li>
+      </ul>
+      <div className="legal">&copy; 2017 by trillo. All rights reserved.</div>
+    </nav>
+  );
 }
 
 export default Nav;
