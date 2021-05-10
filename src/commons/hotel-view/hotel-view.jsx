5b8f1751b769460e1a074b27955c4e40632d587d
Yusuf Ipek <yusufipek@protonmail.com>
2021-05-10 12:29:45 +0300

    created and styled call to action

--- src/commons/hotel-view/hotel-view.jsx
+++ src/commons/hotel-view/hotel-view.jsx
@@ -129,6 +129,16 @@ function HotelView() {
           </button>
         </div>
       </div>
+
+      <div className="cta">
+        <h2 className="cta__book-now">
+          Good news! We have 4 free roms for your selected dates!
+        </h2>
+        <button className="btn">
+          <span className="btn__visible">Book now</span>
+          <span className="btn__invisible">Only 4 rooms left</span>
+        </button>
+      </div>
     </main>
   );
 }
